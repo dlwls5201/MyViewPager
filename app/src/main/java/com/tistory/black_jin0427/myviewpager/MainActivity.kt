dade90dbd92453802ebbb1308a45a0fb7d2a24be
iwedding <>
2018-09-05 14:17:02 +0900

    add indicator

--- app/src/main/java/com/tistory/black_jin0427/myviewpager/MainActivity.kt
+++ app/src/main/java/com/tistory/black_jin0427/myviewpager/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tistory.black_jin0427.myviewpager
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //init viewpager
         vpMainActivity.adapter = MainActivity@adapter
+
+        vpMainActivity.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(p0: Int) {
+
+            }
+
+            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+
+            }
+
+            override fun onPageSelected(p0: Int) {
+                ciMainActivity.selectDot(p0)
+            }
+
+        })
+
+        //init indicator
+        ciMainActivity.createDotPanel(5, R.drawable.indicator_dot_off, R.drawable.indicator_dot_on, 0)
     }
 }
