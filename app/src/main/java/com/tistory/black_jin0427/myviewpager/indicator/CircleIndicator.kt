d99f146568a32a2bb378b07a41e1c46302438c6a
iwedding <>
2018-09-05 14:45:03 +0900

    modify

--- app/src/main/java/com/tistory/black_jin0427/myviewpager/indicator/CircleIndicator.kt
+++ app/src/main/java/com/tistory/black_jin0427/myviewpager/indicator/CircleIndicator.kt
@@ -15,7 +15,7 @@ class CircleIndicator: LinearLayout {
 
     private var imageDot: MutableList<ImageView> = mutableListOf()
 
-    // 4.5 dp 를 픽셀 단위로 바꿉니다.
+    // 4.5dp 를 픽셀 단위로 바꿉니다.
     private val temp = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP, 4.5f, resources.displayMetrics)
 
